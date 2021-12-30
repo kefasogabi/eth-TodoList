0863f516603c2966ba872805a1fdc9c58b309594
Kefas Ogabi <kefasogabi@gmail.com>
2021-12-30 09:22:25 +0100

    Second Commit

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,6 +2,26 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
-    uint taskCount = 0;
+     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content; 
+        bool completed;
+    }
+
+    mapping(uint =>  Task) public tasks;
+
+    constructor() {
+
+        createTask("You wanna see something cool?");
+    }
+
+    function createTask(string memory _content)  public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+
+
+    }
      
 }
